466d492e8f921a51a250d0a08571a62b9705932f
smkelly1691 <skelly1691@gmail.com>
2022-09-21 07:21:22 -0700

    adds cached elements, event listeners, and init function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-  /*----- constants -----*/
+/*----- constants -----*/
+// Selection of words game will choose from at random for the player to guess
 const  WORDS = [
     "javascript", 
     "object",
@@ -6,31 +7,60 @@ const  WORDS = [
     "function",
 ]
 
+// String for each letter of the alphabet to create player choices
 const ALPHABET = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", 
     "j", "k", "l", "m", "n", "o", "p", "q", "r", 
     "s", "t", "u", "v", "w", "x", "y", "z"
 ]
 
-
-  /*----- state variables -----*/
-let word 
-    guesses 
-    numGuesses 
-    numGuessesLeft 
-    numCorrectGuesses
+/*----- state variables -----*/
+let word, 
+    guesses, 
+    numGuesses, 
+    numGuessesLeft, 
+    numCorrectGuesses,
     numChar
 
-  /*----- cached elements  -----*/
+/*----- cached elements  -----*/
 const numGuessesLeftEl = document.getElementById("numGuessesLeft");
 const messageDisplayEl = document.querySelector('h2')
 
 
-  /*----- event listeners -----*/
+/*----- event listeners -----*/
+resetBtnEl.addEventListener("click", handleResetClick)
+letterEl.addEventListener("click", handleLetterClick)
 
+/*----- functions -----*/
+// Create initial game state (Initialize Game)
+function init() {
+    word = ""
+    guesses = [null]
+    numGuesses = 0
+    numGuessesLeft = 10
+    numCorrectGuesses = 0
+    numChar = 0
+    render()
+}
 
-  /*----- functions -----*/
+function render() {
+// Choose a word from the WORDS array at random for the player to guess
+const randomWord = Math.floor(Math.random() * this.WORDS.length);
+return this.WORDS[randomWord];
+ 
+// Fill the letter buttons with the letters of the alphabet 
+function fillLetters() {
+    for(let i=0; i<ALPHABET.length; i++) {
+        ALPHABET[i] = document.querySelectorAll(".letters", i);
+    }
+    
+}
 
+}
 
-  // GAME START
+function handleResetClick() {
+    init()
+}
+
+// GAME START
   init()
